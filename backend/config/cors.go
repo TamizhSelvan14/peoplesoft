79caa551d9a771c7a57e1081de4d9fc76bd43e42
Jaya Vyas <jaya.vyas@sjsu.edu>
2025-12-01 20:11:46 -0800

    Fix Cors

--- backend/config/cors.go
+++ backend/config/cors.go
@@ -4,17 +4,30 @@ import "github.com/gin-gonic/gin"
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		origin := c.Request.Header.Get("Origin")
-		if origin == "http://localhost:5173" || origin == "https://d1coettjn7fksr.cloudfront.net" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		origin := c.GetHeader("Origin")
+
+		allowedOrigins := []string{
+			"http://localhost:5173",
+			"https://d1coettjn7fksr.cloudfront.net",
 		}
+
+		for _, o := range allowedOrigins {
+			if o == origin {
+				c.Writer.Header().Set("Access-Control-Allow-Origin", o)
+				break
+			}
+		}
+
+		c.Writer.Header().Set("Vary", "Origin")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
+
 		c.Next()
 	}
 }
