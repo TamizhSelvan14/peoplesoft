8d7a640bd41595c782feda3db12ede562947341a
Jaya Vyas <jaya.vyas@sjsu.edu>
2025-12-01 19:37:54 -0800

    JenkinsPipeline

--- backend/config/cors.go
+++ backend/config/cors.go
@@ -4,7 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := c.Request.Header.Get("Origin")
+		if origin == "http://localhost:5173" || origin == "https://d1coettjn7fksr.cloudfront.net" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
